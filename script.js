8e892470fcd18b50aca7148ac319e15f1049ef90
dant4ick-free <134090823+dant4ick-free@users.noreply.github.com>
2023-06-12 19:46:42 +0000

    modify page to display received image

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 document.getElementById('upload-button').addEventListener('click', async function() {
-    const imageInput = document.getElementById('image-input');
-    const file = imageInput.files[0];
-  
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = async function() {
-        const imageBase64 = reader.result.split(',')[1];
-  
-        const response = await fetch('https://functions.yandexcloud.net/d4e48qrvft4m7jei77hr', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            image_file: file.name,
-            image_data: imageBase64,
-            image_invert: true // Change this value as needed
-          })
-        });
-  
-        const result = await response.json();
-        document.getElementById('result').textContent = JSON.stringify(result, null, 2);
-      };
-  
-      reader.readAsDataURL(file);
-    }
-  });
-  
+  const imageInput = document.getElementById('image-input');
+  const file = imageInput.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+    reader.onloadend = async function() {
+      const imageBase64 = reader.result.split(',')[1];
+
+      const response = await fetch('https://functions.yandexcloud.net/d4e48qrvft4m7jei77hr', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          image_file: file.name,
+          image_data: imageBase64,
+          image_invert: true // Change this value as needed
+        })
+      });
+
+      const result = await response.json();
+      document.getElementById('result').textContent = JSON.stringify(result, null, 2);
+
+      if (result.uploaded_image_data) {
+        const imageContainer = document.getElementById('image-container');
+        const imageElement = document.createElement('img');
+        imageElement.src = 'data:image/png;base64,' + result.uploaded_image_data;
+        imageContainer.innerHTML = '';
+        imageContainer.appendChild(imageElement);
+      }
+    };
+
+    reader.readAsDataURL(file);
+  }
+});
